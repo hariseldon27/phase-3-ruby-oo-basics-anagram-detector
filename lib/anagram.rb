18a003001cbb01c5792c38d889635bb86ace47c0
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-24 13:05:46 -0800

    commit tree 94d3137006a2ef8efd59243c2432c89f87107927

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+
+    def initialize(word_input)
+        @word_input = word_input
+    end
+    def match(arry_to_check_for_matches)
+        matching = []
+        arry_to_check_for_matches.each do |word|
+            if word.chars.sort == @word_input.chars.sort
+                matching << word
+            end
+            # binding.pry
+        end
+        return matching
+    end
+
+
+end
+# binding.pry
